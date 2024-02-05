7cc9dbcd29b49a65cc9c3ecc9ae19613d5a99041
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-02-05 13:03:43 +0100

    feat(lego): Add contact us page

--- year2/php/lego/routes/web.php
+++ year2/php/lego/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogPostController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', [BlogPostController::class, 'index']);
+
+Route::get('/contact-us', [ContactController::class, 'index']);
