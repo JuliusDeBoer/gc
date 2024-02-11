4bbb165527c4aaae08c9583a4bbfdf3fadc15094
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-02-11 17:51:25 +0100

    feat(lego): Add caching server using lada-cache

--- year2/php/lego/app/Models/Contact.php
+++ year2/php/lego/app/Models/Contact.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spiritix\LadaCache\Database\LadaCacheTrait;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, LadaCacheTrait;
 }
