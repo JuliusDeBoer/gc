10308484d73c78908d401ddeccabf4cf80f67c09
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-03-11 13:18:14 +0100

    refactor(lego): Rename table
    Very profesional

--- year2/php/lego/database/migrations/2024_03_07_131551_create_attractions_table.php
+++ year2/php/lego/database/migrations/2024_03_07_131551_create_attractions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('activities', function (Blueprint $table) {
+        Schema::create('attractions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->text('title');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('activities');
+        Schema::dropIfExists('attractions');
     }
 };
