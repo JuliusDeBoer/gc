71c9fdd2404e7b1ff2a098c328dfe5ef48fbd5fe
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-03-11 13:26:13 +0100

    refactor(ns-api): Delete `ns-api`

--- year2/webdev/ns-api/server/api/[station]/arrivals.ts
+++
@@ -1,9 +0,0 @@
-import { fetchWithHeaders } from "~/composables/fetchWithHeaders";
-
-export default defineEventHandler(async (event) => {
-  const station = getRouterParam(event, "station");
-
-  return fetchWithHeaders(
-    `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/arrivals?station=${station}`,
-  );
-});
--- year2/webdev/ns-api/server/api/places.ts
+++
@@ -1,7 +0,0 @@
-import { fetchWithHeaders } from "~/composables/fetchWithHeaders";
-
-export default defineEventHandler(async () => {
-  return fetchWithHeaders(
-    `https://gateway.apiportal.ns.nl/places-api/v2/places`,
-  );
-});
