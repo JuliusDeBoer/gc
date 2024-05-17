9fad2693a00fcb67c38538979291038a7543ae69
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-05-17 12:57:45 +0200

    delete(y): Moved to https://github.com/JuliusDeBoer/Y

--- year2/webdev/y/frontend/src/components/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@mui/material";
-
-export default function Loading() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center flex-col bg-black">
-      <CircularProgress />
-    </div>
-  );
-}
